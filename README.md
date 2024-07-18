35cb8be632b42be7f4daff0b3fdd43a81d5445fe
Badger Code <83559316+321BadgerCode@users.noreply.github.com>
2024-07-18 18:51:37 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-## TileArt
+## Tile Tinkerer
 I saw an interesting image and decided to re-create it, and then added some extra functionality.
 ### TileArt creates art by repeating a set mask a number of iterations and averaging the mask's color with the existing image's color.
-To use, just update the variables in the top lines of `main.py` and run. 
+## Usage
+```sh
+python ./main.py # Will output the help menu
+# python ./main.py <mask1> <mask2> ...
+python ./main.py \(0x0000ff,0xff0000\) \(0x0000ff,0xff0000\) # Example of how the program takes in hex color values
+```
 
 ![Sample TileArt](https://raw.githubusercontent.com/Somebody32x2/TileArt/master/tileart_%5B000000%2C000080%2C0000ff%5D%2C%5B008000%2Cffffff%2C0080ff%5D%2C%5B00ff00%2C00ff80%2C00ffff%5D_x6.png)
--- main.py
+++ main.py
@@ -1,46 +1,84 @@
+import sys
+import random
 from PIL import Image
 
-mask = [
-    [(000,000,255),(255,000,000)],
-    [(255,000,000),(000,000,255)],
-]
-iterations = 6
+class TxtClr:
+	red = '\033[91m'
+	green = '\033[92m'
+	blue = '\033[94m'
+	reset = '\033[0m'
+
+def clr(hexColor:str)->str:
+	hexColor = [hexColor[i:i+2] for i in range(0, len(hexColor), 2)]
+	return f"0x{TxtClr.red}{hexColor[0]}{TxtClr.green}{hexColor[1]}{TxtClr.blue}{hexColor[2]}{TxtClr.reset}"
+
+def randomClr(max_tuples=3,max_clr=3)->str:
+	num_tuples = random.randint(1, max_tuples)
+	num_colors = random.randint(1, max_clr)
+	colors = ""
+	for i in range(num_tuples):
+		color = []
+		for j in range(num_colors):
+			txt:str=clr(f"{random.randint(0x000000, 0xffffff):06x}")
+			color.append(txt)
+		end = "" if i == num_tuples - 1 else " "
+		colors += f"\({','.join(color)}\){end}"
+	return colors
+
+if len(sys.argv) < 2:
+	print(f"Usage: python {sys.argv[0]} <mask1> <mask2> ...")
+	print(f"Example: python {sys.argv[0]} \(%s,%s\) \(%s,%s\)" % (clr("0000ff"), clr("ff0000"),clr("0000ff"), clr("ff0000")))
+	print("\tWhere mask is an RGB value in hex format")
+	print("")
+	print("If you don't know what mask to use, try this one out:")
+	print(f"\npython {sys.argv[0]} {randomClr()}")
+
+	sys.exit(0)
+
+mask = []*len(sys.argv)
+for i in range(1, len(sys.argv)):
+	mask.append([])
+	rgb = sys.argv[i].replace("0x","").replace("(","").replace(")","").split(",")
+	print(rgb)
+	for j in range(len(rgb)):
+		rgb[j] = tuple(rgb[j][i:i+2] for i in range(0, len(rgb[j]), 2))
+		rgb[j] = tuple(map(lambda x: int(x, 16), rgb[j]))
+		mask[i-1].append(rgb[j])
+print(mask)
+iterations = 3
 pic = mask
 
-
 def tupleScale(t, m):
-    return tuple(map(lambda e: e * m, t))
-
+	return tuple(map(lambda e: e * m, t))
 
 def tupleAdd(a, b):
-    return tuple((a[i] + b[i] for i in range(len(a))))
-
+	return tuple((a[i] + b[i] for i in range(len(a))))
 
 for i in range(iterations):
-    # square the pic
-    npic = [[0 for _ in range(len(pic[0]) * len(mask[0]))] for _ in range(len(pic) * len(mask))]
-    for y in range(len(pic)):
-        for x in range(len(pic[0])):
-            for y1 in range(len(mask)):
-                for x1 in range(len(mask[0])):
-                    npic[y + y1 * len(pic)][x + x1 * len(pic[0])] = tupleAdd(tupleScale(pic[y][x], 0.5), tupleScale(mask[y1][x1], 0.5))
+	# Square the pic
+	npic = [[0 for _ in range(len(pic[0]) * len(mask[0]))] for _ in range(len(pic) * len(mask))]
+	for y in range(len(pic)):
+		for x in range(len(pic[0])):
+			for y1 in range(len(mask)):
+				for x1 in range(len(mask[0])):
+					npic[y + y1 * len(pic)][x + x1 * len(pic[0])] = tupleAdd(tupleScale(pic[y][x], 0.5), tupleScale(mask[y1][x1], 0.5))
 
-    pic = npic
+	pic = npic
 
-# convert to grayscale picture and show
+# Convert to grayscale pic and show
 im = Image.new("RGB", (len(pic[0]), len(pic)))
 
 for y in range(len(pic)):
-    for x in range(len(pic[0])):
-        im.putpixel((x, y), tuple(map(int, pic[y][x])))
+	for x in range(len(pic[0])):
+		im.putpixel((x, y), tuple(map(int, pic[y][x])))
 
 # Generate a name which encodes the hex vals of the mask
 name = "tileart_"
 for x in range(len(mask)):
-    name += "["
-    for y in range(len(mask[0])):
-        name += "{:02x}{:02x}{:02x}".format(mask[x][y][0], mask[x][y][1], mask[x][y][2]) + ("," if y != len(mask[0]) - 1 else "")
-    name += "]" + ("," if x != len(mask) - 1 else "")
+	name += "["
+	for y in range(len(mask[0])):
+		name += "{:02x}{:02x}{:02x}".format(mask[x][y][0], mask[x][y][1], mask[x][y][2]) + ("," if y != len(mask[0]) - 1 else "")
+	name += "]" + ("," if x != len(mask) - 1 else "")
 name += "_" + "x" + str(iterations) + ".png"
 im.save(name)
 im.show()
